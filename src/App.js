63fcf2f7012dcd350c59872507672494c49957dc
silvinomar <silvinomartins991@gmail.com>
2023-06-06 12:16:31 +0100

    first. project v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import PlayerRow from './PlayerRow';
 
-function App() {
+const url = "https://ratings.fide.com/profile/";
+const ids = [
+  1976508, 14173328, 1928660, 1971891, 1970720, 1967495, 1972332, 1914995, 1910116, 1926888, 1976680, 1964585, 1924893, 16267214, 1915312, 1939823, 1912704, 1922610, 1922629, 1965263, 1976656, 1969323, 1976494, 1969145, 1970399, 1976613, 1976672, 1937839, 1976648, 34173153, 1929801, 1976699, 1954539
+];
+
+const App = () => {
+  const [playersData, setPlayersData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [sortColumn, setSortColumn] = useState("classical");
+
+  useEffect(() => {
+    fetchPlayersData();
+  }, []);
+
+  useEffect(() => {
+    sortPlayersData(sortColumn);
+  }, [playersData]);
+
+  const fetchPlayersData = async () => {
+    try {
+      const promises = ids.map(id => axios.get(url + id));
+      const responses = await Promise.all(promises);
+      const playersData = responses.map(response => {
+        const $ = cheerio.load(response.data);
+        const name = $('.profile-top-title').text();
+
+        let classical_rating = $('.profile-top-rating-data_gray').text();
+        if (!classical_rating.includes("Not rated")) {
+          classical_rating = classical_rating.replace(/\s+/g, '').substring(3);
+        } else {
+          classical_rating = "Not rated";
+        }
+
+        let rapid_rating = $('.profile-top-rating-data_red').text();
+        if (!rapid_rating.includes("Not rated")) {
+          rapid_rating = rapid_rating.replace(/\s+/g, '').substring(5);
+        } else {
+          rapid_rating = "Not rated";
+        }
+
+        let blitz_rating = $('.profile-top-rating-data_blue').text();
+        if (!blitz_rating.includes("Not rated")) {
+          blitz_rating = blitz_rating.replace(/\s+/g, '').substring(5);
+        } else {
+          blitz_rating = "Not rated";
+        }
+
+        return {
+          id: response.config.url.substring(url.length),
+          name,
+          classical: classical_rating,
+          rapid: rapid_rating,
+          blitz: blitz_rating
+        };
+      });
+
+      setPlayersData(playersData);
+      setIsLoading(false);
+
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+
+  const sortPlayersData = (column) => {
+
+    if (sortColumn !== column) {
+
+    setPlayersData(prevPlayersData => [...prevPlayersData].sort((a, b) => {
+      if (a[column] === "Not rated") {
+        return 1;
+      } else if (b[column] === "Not rated") {
+        return -1;
+      } else {
+        return b[column] - a[column];
+      }
+      setSortColumn(column);
+    
+    }));}
+  };
+
+  const sorting = (column) => {
+    if (column === sortColumn) {
+      setPlayersData([...playersData].reverse());
+    } else {
+      setSortColumn(column);
+      sortPlayersData(column);
+    }
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='container'>
+      <h1 className="my-2">Musas live FIDE ratings</h1>
+
+      <table id="playerTable" className='table mx-auto'>
+        <thead className='sticky-top bg-white'>
+          <tr>
+            <th>Player</th>
+            <th onClick={() => sorting("classical")} className={sortColumn === "classical" ? "active" : ""}>Classical</th>
+            <th onClick={() => sorting("rapid")} className={sortColumn === "rapid" ? "active" : ""}>Rapid</th>
+            <th onClick={() => sorting("blitz")} className={sortColumn === "blitz" ? "active" : ""}>Blitz</th>
+          </tr>
+        </thead>
+        <tbody>
+          {playersData.map(player => (
+            <PlayerRow
+              key={player.id}
+              id={player.id}
+              name={player.name}
+              classical={player.classical}
+              rapid={player.rapid}
+              blitz={player.blitz}
+            />
+          ))}
+        </tbody>
+      </table>
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/PlayerRow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PlayerRow = (props) => {
+    return (
+        <tr>
+            <td><a href={"https://ratings.fide.com/profile/"+props.id} target='_blank'>{props.name}</a></td>
+            <td>{props.classical}</td>
+            <td>{props.rapid}</td>
+            <td>{props.blitz}</td>
+        </tr>
+    )
+}
+
+export default PlayerRow
