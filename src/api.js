60d76039d0f5a881deb29f3a80360582775047d3
silvinomar <silvinomartins991@gmail.com>
2023-12-11 10:25:32 +0000

    removed old api endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,4 @@
-// src/api.js
 const API_BASE_URL = 'https://energetic-tan-monkey.cyclic.app/';
-//const API_BASE_URL = 'https://reliable-lavish-larch.glitch.me';
-
 
 export const fetchPlayerInfo = async (fideNumber, includeHistory) => {
   const response = await fetch(`${API_BASE_URL}/player/${fideNumber}/info?include_history=${includeHistory}`);
