6cb837f06c6af98f25573eb29931fefd5fdae608
silvinomar <silvinomartins991@gmail.com>
2023-12-11 21:25:30 +0000

    added variations

--- src/components/PlayerRow.js
+++ src/components/PlayerRow.js
@@ -3,10 +3,10 @@ import React from 'react'
 const PlayerRow = (props) => {
     return (
         <tr>
-            <td><a className="" href={"https://ratings.fide.com/profile/"+props.id} target='_blank'>{props.name}</a></td>
-            <td>{props.classical} <span className="small">{props.c_variation}</span></td>
-            <td>{props.rapid} <span className="small">{props.r_variation}</span></td>
-            <td>{props.blitz} <span className="small">{props.b_variation}</span></td>
+            <td><a className="" href={"https://ratings.fide.com/profile/"+props.id} target='_blank' rel='noreferrer'>{props.name}</a></td>
+            <td>{props.classical} <span className={"small "+(props.c_variation>0)}>{props.c_variation}</span></td>
+            <td>{props.rapid} <span className={"small "+(props.r_variation>0)}>{props.r_variation}</span></td>
+            <td>{props.blitz} <span className={"small "+(props.b_variation>0)}>{props.b_variation}</span></td>
         </tr>
     )
 }
