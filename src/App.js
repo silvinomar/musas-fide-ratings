ad8aaa2490ea3cf87e59360afd80a5a31dc77835
silvinomar <silvinomartins991@gmail.com>
2024-11-17 16:41:09 +0000

    display last update date on footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PlayerStats from './components/PlayerStats';
+import musasData from './data/musas-data.json'; 
 
 function App() {
+
+    const lastUpdate = Object.values(musasData)?.[0]?.fetchDate || null;
+    // Format date "YYYY-MM-DD"
+    const formattedDate = lastUpdate ? new Date(lastUpdate).toISOString().split('T')[0] : 'N/A';
+  
   return (
     <div>
       <PlayerStats />
       <footer>
         <p>User interface to display FIDE ratings from <a href="http://musas.pegada.net/" target='_blank' rel='noreferrer'>Musas</a> players by <a href="https://github.com/silvinomar" target="_blank" rel="noreferrer">silvinomar</a>.</p>
         <p>Data retrieved with the <a href="https://github.com/xRuiAlves/fide-ratings-scraper" target='_blank' rel="noreferrer">FIDE ratings scraper</a> by Rui Alves. </p>
+        <p>Last update: <b>{formattedDate}</b></p>
       </footer>
     </div>
   );
