bde8ba8aebbb0ec6b215f1194ba2f63ebd5f4848
silvinomar <silvinomartins991@gmail.com>
2025-04-12 20:03:04 +0100

    april ratings

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
       <PlayerStats />
       <footer>
         <p>User interface to display FIDE ratings from <a href="https://espacomusas.pt/" target='_blank' rel='noreferrer'>Musas</a> players by <a href="https://github.com/silvinomar" target="_blank" rel="noreferrer">silvinomar</a>.</p>
-        <p>Data retrieved with the <a href="https://github.com/xRuiAlves/fide-ratings-scraper" target='_blank' rel="noreferrer">FIDE ratings scraper</a> by Rui Alves. </p>
         <p>Last update: <b>{formattedDate}</b></p>
       </footer>
     </div>
